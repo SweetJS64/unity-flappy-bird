8b8d6d410d415c6e1528909da84e7618b6a2356b
SweetJS64 <miroran64@gmail.com>
2025-10-08 21:19:09 +0400

    Fix: Flap after pause

--- Assets/Game/Infrastructure/DesktopInputService.cs
+++ Assets/Game/Infrastructure/DesktopInputService.cs
@@ -1,12 +1,18 @@
 using Game.Core;
 using UnityEngine;
+using Zenject;
 
 namespace Game.Infrastructure
 {
     public class DesktopInputService : IInputService
     {
+        [Inject] private IGameSession _session;
+        
         public bool IsJumpPressed()
         {
+            if (_session.State.Value != GameState.Playing && _session.State.Value != GameState.Idle)
+                return false;
+            
             return Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space);
         }
     }
--- Assets/Game/Infrastructure/MobileInputService.cs
+++ Assets/Game/Infrastructure/MobileInputService.cs
@@ -1,12 +1,16 @@
 using Game.Core;
 using UnityEngine;
+using Zenject;
 
 namespace Game.Infrastructure
 {
     public class MobileInputService : IInputService
     {
+        [Inject] private IGameSession _session;
         public bool IsJumpPressed()
         {
+            if (_session.State.Value != GameState.Playing && _session.State.Value != GameState.Idle)
+                return false;
             if (Input.touchCount <= 0) return false;
             var t = Input.GetTouch(0);
             return t.phase == TouchPhase.Began;
